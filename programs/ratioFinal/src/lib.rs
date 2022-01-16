6e8393c4b30f32392f9e936a11b9387a6c740ac4
Mgiar2015 <mgiar2015@gmail.com>
2022-01-16 17:12:54 -0500

    Initial commit

---
+++ programs/ratioFinal/src/lib.rs
@@ -0,0 +1,125 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{Mint, Token, TokenAccount},
+};
+
+declare_id!("EUAi4FePMFtT5QCxyRCJjPiHZCZQPjkQ7xZzCacMNfvc");
+
+#[program]
+pub mod ratio {
+    use super::*;
+    pub fn deposit(ctx: Context<Deposit>, _bump: u8, amount: u64) -> ProgramResult {
+        anchor_spl::token::transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                anchor_spl::token::Transfer {
+                    from: ctx
+                        .accounts
+                        .payer_token_account
+                        .to_account_info(),
+                    to: ctx
+                        .accounts
+                        .vault_account
+                        .to_account_info(),
+                    authority: ctx.accounts.payer.to_account_info(),
+                },
+            ),
+            amount,
+        )
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, bump: u8, amount: u64) -> ProgramResult {
+        anchor_spl::token::transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                anchor_spl::token::Transfer {
+                    from: ctx
+                        .accounts
+                        .vault_account
+                        .to_account_info(),
+                    to: ctx
+                        .accounts
+                        .payer_token_account
+                        .to_account_info(),
+                    authority: ctx.accounts.vault_account.to_account_info(),
+                },
+                &[&[
+                    ctx.accounts.mint.key().as_ref(),
+                    &[bump],
+                ]],
+            ),
+            amount,
+        )
+    }
+
+    pub fn airdrop(ctx: Context<Airdrop>,mint_seed: Vec<u8>, mint_bump: u8, amount: u64) -> ProgramResult {
+        anchor_spl::token::mint_to(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                anchor_spl::token::MintTo {
+                    mint: ctx.accounts.mint.to_account_info(),
+                    to: ctx.accounts.destination.to_account_info(),
+                    authority: ctx.accounts.payer.to_account_info(),
+                }
+            ),
+            amount,
+        )
+    }
+
+}
+
+#[derive(Accounts)]
+#[instruction(bump: u8)]
+pub struct Deposit<'info> {
+    #[account(init_if_needed,
+        payer = payer,
+        seeds = [mint.key().as_ref()],
+        bump = bump,
+        token::mint = mint,
+        token::authority = vault_account)]
+    pub vault_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(mut)]
+    pub payer_token_account: Account<'info, TokenAccount>,
+    pub mint: Account<'info, Mint>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub vault_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(mut)]
+    pub payer_token_account: Account<'info, TokenAccount>,
+    pub mint: Account<'info, Mint>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+}
+
+//#[instruction(mint_seed: Vec<u8>,mint_bump: u8, amount: u64)]
+#[derive(Accounts)]
+pub struct Airdrop<'info> {
+    #[account(mut)]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub destination: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+
+
+
+
+
+
---
+++ tests/ratioFinal.ts
@@ -0,0 +1,122 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { RatioFinal } from '../target/types/ratio_final';
+import * as spl from '@solana/spl-token';
+
+
+describe('ratioFinal', () => {
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+  //program which isused throughout our tests
+  const program = anchor.workspace.Ratio as Program<RatioFinal>;
+
+  let mintAccount: anchor.web3.Keypair;
+  let userAccount: anchor.web3.Keypair;
+  let testUserTokenAccount: anchor.web3.PublicKey;
+  let pdaTokenAAddress = null;
+  let pdaTokenABump = null;
+  let tokenMint: spl.Token;
+
+  before(async () => {
+    //account which is the owner of the mint, airdrop it lamports to create mint
+    mintAccount = anchor.web3.Keypair.generate();
+    var fromAirDropSignature = await program.provider.connection.requestAirdrop(mintAccount.publicKey,1000000000)
+    await program.provider.connection.confirmTransaction(fromAirDropSignature);
+    //system wallet with key wallet.publicKey.toBase58()
+    const wallet = program.provider.wallet;
+
+
+
+    userAccount = anchor.web3.Keypair.generate();
+    var fromAirDropSignature = await program.provider.connection.requestAirdrop(userAccount.publicKey,1000000000)
+    await program.provider.connection.confirmTransaction(fromAirDropSignature);
+
+    //account of mint for our new token
+    tokenMint = await spl.Token.createMint(program.provider.connection,
+      userAccount, //payer
+      userAccount.publicKey, //mint auth
+      null, //freeze auth
+      0, //decimals
+      spl.TOKEN_PROGRAM_ID); //programID for spl token program, which is a universal contract
+    //create account for our user to hold their tokens in
+    testUserTokenAccount = await tokenMint.createAssociatedTokenAccount(
+      userAccount.publicKey
+    )
+    //mint 100 test tokens in our test users token account
+    await tokenMint.mintTo(testUserTokenAccount, userAccount.publicKey, [], 100);
+    console.log("Setup Complete")
+  })
+
+  it('It deposits and withdraws!', async () => {
+    // Add your test here.
+    [pdaTokenAAddress, pdaTokenABump] = await anchor.web3.PublicKey.findProgramAddress([tokenMint.publicKey.toBuffer()], program.programId)
+    console.log(`PDA Token A Address: ${pdaTokenAAddress}, Bump: ${pdaTokenABump}`);
+    [pdaTokenAAddress, pdaTokenABump] = await anchor.web3.PublicKey.findProgramAddress([tokenMint.publicKey.toBuffer()], program.programId)
+    console.log(`PDA Token A Address: ${pdaTokenAAddress}, Bump: ${pdaTokenABump}`);
+    
+    const tx = await program.rpc.deposit(pdaTokenABump, new anchor.BN(10),
+      {
+        accounts: {
+          vaultAccount: pdaTokenAAddress,
+          payer: userAccount.publicKey,
+          payerTokenAccount:testUserTokenAccount,
+          mint: tokenMint.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          tokenProgram: spl.TOKEN_PROGRAM_ID,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        },
+      signers: [userAccount]
+    });
+
+    const tx2 = await program.rpc.withdraw(pdaTokenABump, new anchor.BN(5),{
+      accounts: {
+        vaultAccount: pdaTokenAAddress,
+        payer: userAccount.publicKey,
+        payerTokenAccount:testUserTokenAccount,
+        mint: tokenMint.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        tokenProgram: spl.TOKEN_PROGRAM_ID,
+      },
+      signers: [userAccount]
+    });
+
+    const tokenSeed = Buffer.from(anchor.utils.bytes.utf8.encode("pig-mint-faucet"));
+    const [tokenMintPda, tokenMintPdaBump] = await anchor.web3.PublicKey.findProgramAddress(
+      [tokenSeed],
+      program.programId);
+
+    let associatedPigTokenAccount = await spl.Token.getAssociatedTokenAddress(
+      spl.ASSOCIATED_TOKEN_PROGRAM_ID,
+      spl.TOKEN_PROGRAM_ID,
+      tokenMintPda,
+      program.provider.wallet.publicKey,
+    );
+  
+
+    const tx3 =  await program.rpc.airdrop(
+      tokenSeed,
+      tokenMintPdaBump,
+      new anchor.BN(10),
+      {
+        accounts: {
+          payer: userAccount.publicKey,
+          mint: tokenMint.publicKey,
+          destination: pdaTokenAAddress,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+          tokenProgram: spl.TOKEN_PROGRAM_ID,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          associatedTokenProgram: spl.ASSOCIATED_TOKEN_PROGRAM_ID,
+        },
+        signers: [userAccount],
+      }
+    );
+
+    let amountInVault = (await tokenMint.getAccountInfo(pdaTokenAAddress)).amount.toNumber();
+    let amountInWallet = (await tokenMint.getAccountInfo(testUserTokenAccount)).amount.toNumber();
+    console.log(amountInVault)
+    console.log(amountInWallet)
+
+    console.log("Your transaction signature", tx);
+  });
+});
